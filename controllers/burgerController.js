cfec05e0ea8c539b83362e668450924462d60645
sjmars <sjmarsnc@gmail.com>
2020-02-18 11:28:53 -0500

    remove some debugging console.log calls

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -11,13 +11,11 @@ router.get("/", function(req, res) {
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  console.log(req); 
   burger.create([
     "name", "devoured"
   ], [
@@ -30,8 +28,6 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
